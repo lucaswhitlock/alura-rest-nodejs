7230dc7b449f7d270bbfa160234f3b33ba1d132a
lwhitlock <lwhitlock@fazenda.ms.gov.br>
2021-04-02 17:20:38 -0400

    Alterações nas rotas para uso de autenticação com tokens e uso do Redis para blacklist de tokens no logout

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
       script: "server.js",
       env_dev: {
         "NODE_ENV": "dev",
-        "PORT": 3000
+        "PORT": 3000,
+        "JWT_SECRET": "417530414128f74c0e3067622041f33a8b1c66fcf2dc8d45672ede3db949dfaa"
       }
     }
   ]
